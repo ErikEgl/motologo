52aee2b14a4aa95e3657aa772eb289d52f1043bd
Erik Egliens <mr.erik.egliens@gmail.com>
2020-11-12 04:02:16 +0200

    test

--- js/main.js
+++ js/main.js
@@ -11,6 +11,28 @@ var swiper = new Swiper('.swiper__bikeSlider', {
         nextEl: '.ico-arr-right',
         prevEl: '.ico-arr-left',
       },
+      breakpoints: {
+           // when window width is >= 240px
+           240: {
+            slidesPerView: 1,
+    
+          },
+        // when window width is >= 320px
+        320: {
+          slidesPerView: 1,
+  
+        },
+        // when window width is >= 480px
+        480: {
+          slidesPerView: 1,
+  
+        },
+        // when window width is >= 640px
+        640: {
+          slidesPerView: 2,
+  
+        }
+      }
       
   });
 
@@ -35,16 +57,49 @@ var swiper = new Swiper('.swiper__bikeSlider', {
       },
       // when window width is >= 480px
       480: {
-        slidesPerView: 3,
+        slidesPerView: 2,
 
       },
       // when window width is >= 640px
       640: {
-        slidesPerView: 3,
+        slidesPerView: 2,
 
       }
     }
   });
 
+  // Menu opener hamburger
 
+
+
+
+
+
+
+const burger = document.querySelector('.js-burger');
+const body = document.querySelector('body');
+
+burger.addEventListener('click', e => {
+    if (!body.classList.contains('menuMobActive')) {
+        body.classList.add('menuMobActive')
+    } else {
+        body.classList.remove('menuMobActive')
+    }
+});
+
+
+
+function addMenuActiveClass() {
+
+    if (window.scrollY > 0) {
+        body.classList.add('js-active-header');
+    } else {
+        body.classList.remove('js-active-header');
+    }
+}
+
+
+addMenuActiveClass();
+
+window.addEventListener('scroll', addMenuActiveClass);
 
