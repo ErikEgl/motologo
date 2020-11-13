ba0b253ac710b20ba12cf8e33d53f511f6971456
Erik Egliens <mr.erik.egliens@gmail.com>
2020-11-13 04:18:51 +0200

    test

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+const body = document.querySelector('body');
+
+
+
+
 var swiper = new Swiper('.swiper__bikeSlider', {
     slidesPerView: 3,
     centeredSlides: true,
@@ -50,20 +55,30 @@ var swiper = new Swiper('.swiper__bikeSlider', {
         prevEl: '.ico-arr-left',
     },
     breakpoints: {
+      // when window width is >= 280
+      280: {
+        slidesPerView: 2,
+        centeredSlides: false,
+      },
       // when window width is >= 320px
+      
       320: {
         slidesPerView: 2,
-
+        centeredSlides: false,
       },
       // when window width is >= 480px
       480: {
         slidesPerView: 2,
-
+        centeredSlides: false,
       },
       // when window width is >= 640px
       640: {
         slidesPerView: 2,
-
+        centeredSlides: false,
+      },
+      850: {
+        slidesPerView: 3,
+        centeredSlides: true,
       }
     }
   });
@@ -73,11 +88,18 @@ var swiper = new Swiper('.swiper__bikeSlider', {
 
 
 
+// Look for .hamburger
+var hamburger = document.querySelector(".hamburger");
+// On click
+hamburger.addEventListener("click", function() {
+  // Toggle class "is-active"
+  hamburger.classList.toggle("is-active");
+  // Do something else, like open/close menu
+});
 
 
+const burger = document.querySelector('.hamburger');
 
-const burger = document.querySelector('.js-burger');
-const body = document.querySelector('body');
 
 burger.addEventListener('click', e => {
     if (!body.classList.contains('menuMobActive')) {
@@ -89,17 +111,48 @@ burger.addEventListener('click', e => {
 
 
 
-function addMenuActiveClass() {
 
-    if (window.scrollY > 0) {
-        body.classList.add('js-active-header');
-    } else {
-        body.classList.remove('js-active-header');
-    }
+function addMenuActiveClass(scrollHeight, activeClass) {
+  if (window.scrollY > scrollHeight) {
+      body.classList.add(activeClass);
+  } else {
+      body.classList.remove(activeClass);
+  }
 }
 
 
-addMenuActiveClass();
+addMenuActiveClass(0, 'js-active-header');
 
-window.addEventListener('scroll', addMenuActiveClass);
+window.addEventListener('scroll', e=>{  
+  addMenuActiveClass(0, 'js-active-header')
+});
 
+addMenuActiveClass(146, 'js-fixed-header');
+
+window.addEventListener('scroll', e=>{
+addMenuActiveClass(146, 'js-fixed-header')
+});
+
+
+
+onclick= display()
+
+function display() {
+  document.getElementById("myID").style.marginTop = "80px";
+}
+
+
+// function addMenuActiveClass(scrollHeight, activeClass) {
+//   console.log(window.scrollY)
+//   if (window.scrollY > scrollHeight) {
+//       body.classList.remove(activeClass);
+//   } else {
+//       body.classList.add(activeClass);
+//   }
+// }
+
+// addMenuActiveClass(0, 'js-notActive-header');
+
+// window.addEventListener('scroll', e=>{  
+//   addMenuActiveClass(0, 'js-notActive-header')
+// });
